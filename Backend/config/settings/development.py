81ec801b9d5eedcb80ee1b377556536799ad4219
Ryu-cmyk <bhusalaayush207@gmail.com>
2026-03-29 01:57:24 +0000

    railway deployment setup

--- Backend/config/settings/development.py
+++ Backend/config/settings/development.py
@@ -1,9 +1,21 @@
 from .base import *
+import dj_database_url
+import os
 
 DEBUG = True
 
 # Development specific settings
 ALLOWED_HOSTS = ['*']
+
+# Override database with Railway PostgreSQL
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
+
+# Override secret key from Railway environment
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Add browsable API in development
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
@@ -19,5 +31,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Debug toolbar (optional)
 INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE #for railway
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = ['127.0.0.1']
+
+# Override logging for Railway (console only)
+LOGGING['root']['handlers'] = ['console']
+LOGGING['loggers']['django']['handlers'] = ['console']
+LOGGING['loggers']['apps']['handlers'] = ['console']
